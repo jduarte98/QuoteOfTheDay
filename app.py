3ab5e02b6ad270d0b74eb8668f653ee878d2547e
João Duarte <71460038+jduarte98@users.noreply.github.com>
2022-10-29 23:16:35 +0100

    Simplified Quote of The Day Program Finished

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 
 from dbm.dumb import _Database
 from DatabaseConn import DatabaseConn
+import random
+import numpy
+
 
 #Database Connection
 database = DatabaseConn()
 
 print("QUOTE OF THE DAY - 2022")
-# Fetch all the ID's from the quotes table
 database.cursor.execute("SELECT id FROM quotes;")
-ids = database.cursor.fetchall()
+database.cursor.execute("SELECT author, message FROM quotes WHERE id='" + str(random.randint(1, numpy.asarray(database.cursor.fetchall()).size)) + "';")
+retrived_quote = database.cursor.fetchall();
 
-for x in ids:
-    print(x)
+# [('Steve Jobs', '"Your time is limited, so don\'t waste it living someone else\'s life"')]
+
+print(retrived_quote)
+
